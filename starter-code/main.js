8638862352deab2efc58b64cb14a88b621c704a2
Mercedes Egido <meegido@gmail.com>
2017-09-05 19:50:30 +0200

    by name february 2004

--- starter-code/main.js
+++ starter-code/main.js
@@ -89,6 +89,17 @@ mongoClient.connect(url, (error, db) => {
 							}
 						})
             break;
+					case "4":
+					db.collection('companies').find({founded_month: 09}, {name: 1, _id: 0}).toArray((error, result) => {
+						if (error) {
+						console.log(error);
+						rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+							} else {
+								console.log(result);
+								rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+							}
+						})
+            break;
           case "0":
             console.log(`👋👋👋👋 😞 \n`);
             db.close((error) => { process.exit(0) });
