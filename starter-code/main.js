3b68a580bed69e9628d561514d650640cfdb871c
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-06 09:09:19 +0200

    unos cuantos mas

--- starter-code/main.js
+++ starter-code/main.js
@@ -149,12 +149,85 @@ mongoClient.connect(url, (error, db) => {
           });
           break;
         case "7":
-          break;
+				db.collection('companies').find({}, {
+					name: 1,
+					number_of_employees: 1,
+					_id: 0
+				}).sort({'number_of_employees':-1}).limit(10).toArray((error, result) => {
+					if (error) {
+						console.log(error);
+						rl.question(`\nType enter to continue: `, (answer) => {
+							mainMenu();
+						});
+					} else {
+						console.log(result);
+						rl.question(`\nType enter to continue: `, (answer) => {
+							mainMenu();
+						});
+					}
+				});
+				break;
         case "8":
+          db.collection('companies').find({
+            "name": "Facebook"
+          }, {
+            name: 1,
+            _id: 0
+          }).toArray((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => {
+                mainMenu();
+              });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => {
+                mainMenu();
+              });
+            }
+          });
           break;
-        case "9":
-          break;
+				case "9":
+				  db.collection('companies').find({
+				    "name": "Facebook"
+				  }, {
+						name: 1,
+				    number_of_employees: 1,
+				    _id: 0
+				  }).toArray((error, result) => {
+				    if (error) {
+				      console.log(error);
+				      rl.question(`\nType enter to continue: `, (answer) => {
+				        mainMenu();
+				      });
+				    } else {
+				      console.log(result);
+				      rl.question(`\nType enter to continue: `, (answer) => {
+				        mainMenu();
+				      });
+				    }
+				  });
+				  break;
         case "10":
+          db.collection('companies').find({
+            "name": "Facebook"
+          }, {
+            name: 1,
+            products: 1,
+            _id: 0
+          }).toArray((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => {
+                mainMenu();
+              });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => {
+                mainMenu();
+              });
+            }
+          });
           break;
         case "11":
           break;
