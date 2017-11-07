e61f4d4f8bee5e9085fa47d9df310a6625cb80ee
Alber Pedraza <alberpedrazaes@gmail.com>
2017-11-07 17:50:41 +0100

    init daily

---
+++ starter-code/main.js
@@ -0,0 +1,20 @@
+const MongoDB = require('mongodb');
+const mongoClient = MongoDB.MongoClient;
+const clear = require('clear');
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+const url = `mongodb://localhost:27017/crunchbase`;
+
+mongoClient.connect(url, (error, db) => {
+  if (error) {
+    console.log('Error trying to connect to the Database');
+    console.log(error);
+  } else {
+    console.log('Connection established correctly!! 😬');
+
+  }
+});
