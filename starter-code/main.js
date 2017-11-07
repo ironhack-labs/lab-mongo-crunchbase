fa13af4b1176a7461a594d0945cfdcb284fa879f
pablohk <pablusmmid@gmail.com>
2017-11-07 20:43:15 +0100

    query case 4

--- starter-code/main.js
+++ starter-code/main.js
@@ -15,29 +15,92 @@ mongoClient.connect(url, (error, db) => {
     console.log(error);
   } else {
     console.log('Connection established correctly!! 😬');
-    function mainMenu(){
+
+    function mainMenu() {
       clear();
       printMenu();
       rl.question('Type an option: ', (option) => {
-        switch(option){
+        switch (option) {
           case "1":
             console.log('you typed 1');
-            rl.question(`\nType enter to continue: `, (answer) => {mainMenu()});
+            db.collection('companies').find({}, {name: 1,_id: 0}).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu()
+                });
+              }
+            });
             break;
           case "2":
             console.log('you typed 2');
-            rl.question(`\nType enter to continue: `, (answer) => {mainMenu()});
+            db.collection('companies').find({}, {name: 1,_id: 0}).count((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              }
+            });
             break;
+            case "3":
+            console.log('you typed 3');
+            db.collection('companies').find({"founded_year":2004}).count((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              }
+            });
+            break;
+            case "4":
+            console.log('you typed 4');
+            db.collection('companies').find({"founded_year":2004},{name: 1,_id: 0}).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              }
+            });
+            break;
+            
+
+
+
           case "0":
             console.log(`👋👋👋👋 😞 \n`);
-            db.close((error) => { process.exit(0)});
+            db.close((error) => {
+              process.exit(0)
+            });
             break;
           default:
             mainMenu();
             break;
         }
       });
-	}
+    }
 
     mainMenu();
   }
