eb20e9998e57ea225e79e7dba0faaa2d63b1bd36
diana-91 <diana.hervasmoya@gmail.com>
2018-01-05 13:21:52 +0100

    querys: 1,2,3.4

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,5 @@
 const MongoDB = require('mongodb');
+const companies = 'companies';
 const mongoClient = MongoDB.MongoClient;
 const clear = require('clear');
 const readline = require('readline');
@@ -16,22 +17,70 @@ mongoClient.connect(url, (error, db) => {
   } else {
     console.log('Connection established correctly!! 😬');
 
-  }
-});
-
-function mainMenu(){
+    function mainMenu(){
       clear();
       printMenu();
       rl.question('Type an option: ', (option) => {
         switch(option){
           case "1":
-            console.log('you typed 1');
-            rl.question(`\nType enter to continue: `, (answer) => {mainMenu()});
+						db.collection(companies).find({}, {name: 1, _id: 0}).toArray((error, result) => {
+							if (error) {
+								console.log(error);
+								rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+							} else {
+								console.log(result);
+								rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+							}
+						});
             break;
           case "2":
-            console.log('you typed 2');
-            rl.question(`\nType enter to continue: `, (answer) => {mainMenu()});
+						db.collection(companies).count((error,result) => {
+							if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              }
+						});
             break;
+					case "3":
+						db.collection(companies).find({founded_year: 2004},{name:1, founded_year:1, _id:0}).toArray((error,result) => {
+							if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              }
+						})
+						break;
+					case "4":
+						db.collection(companies).find({founded_year: 2004, founded_month:2},{name:1, founded_year:1, founded_month:1, _id:0}).toArray((error,result) => {
+						if (error) {
+							console.log(error);
+							rl.question(`\nType enter to continue: `, (answer) => {
+								mainMenu();
+							});
+						} else {
+							console.log(result);
+							rl.question(`\nType enter to continue: `, (answer) => {
+								mainMenu();
+							});
+						}
+					});
+						break;
+					case "5":
+						
+						break;
           case "0":
             console.log(`👋👋👋👋 😞 \n`);
             db.close((error) => { process.exit(0) });
