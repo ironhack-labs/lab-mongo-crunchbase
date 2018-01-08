49a9264b5bd569f0f2126cb24e0c192a0a976b8c
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-01-08 10:59:18 +0100

    Feat: Case 6

--- starter-code/main.js
+++ starter-code/main.js
@@ -15,7 +15,7 @@ mongoClient.connect(url, (error, db) => {
     console.log(error);
   } else {
     console.log('Connection established correctly!! 😬');
-    
+
     function mainMenu() {
       clear();
       printMenu();
@@ -28,7 +28,10 @@ mongoClient.connect(url, (error, db) => {
             });
             break;
           case "1":
-            db.collection('companies').find({}, {name: 1, _id: 0}).toArray((error, result) => {
+            db.collection('companies').find({}, {
+              name: 1,
+              _id: 0
+            }).toArray((error, result) => {
               if (error) {
                 console.log(error);
                 rl.question(`\nType enter to continue: `, (answer) => {
@@ -58,8 +61,13 @@ mongoClient.connect(url, (error, db) => {
             });
             break;
           case "3":
-          db.collection('companies').find({founded_year: 2004}, {founded_year: 2004, _id: 0}).count((error, result) => {
-            // db.collection('companies').count((error, result) => {
+            db.collection('companies').find({
+              founded_year: 2004
+            }, {
+              founded_year: 2004,
+              _id: 0
+            }).count((error, result) => {
+              // db.collection('companies').count((error, result) => {
               if (error) {
                 console.log(error);
                 rl.question(`\nType enter to continue: `, (answer) => {
@@ -73,10 +81,63 @@ mongoClient.connect(url, (error, db) => {
               }
             });
             break;
-            // 4.- List by name all companies founded in february of 2004.
           case "4":
-          db.collection('companies').find({founded_year: 2004, founded_month:2}, {name:1, _id: 0}).toArray((error, result) => {
-            // db.collection('companies').count((error, result) => {
+            db.collection('companies').find({
+              founded_year: 2004,
+              founded_month: 2
+            }, {
+              name: 1,
+              _id: 0
+            }).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              }
+            });
+            break;
+          case "5":
+            db.collection('companies').find({
+              founded_year: 2004,
+              founded_month: {
+                $gte: 3,
+                $lte: 6
+              }
+            }, {
+              name: 1,
+              founded_year: 1,
+              founded_month: 1,
+              _id: 0
+            }).sort({founded_month:1}).toArray((error, result) => {
+              // db.collection('companies').count((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => {
+                  mainMenu();
+                });
+              }
+            });
+            break;
+            //6.- What companies have offices in "Barcelona".
+          case "6":
+            db.collection('companies').find({
+              "offices.city":"Barcelona"
+            }, {
+              name: 1,
+              _id: 0
+            }).toArray((error, result) => {
+              // db.collection('companies').count((error, result) => {
               if (error) {
                 console.log(error);
                 rl.question(`\nType enter to continue: `, (answer) => {
