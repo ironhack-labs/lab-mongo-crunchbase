a1fc1185d1cc8059a9a4189384d5e24328bf818f
Cristina Bertrand <cristina@11870.com>
2018-01-12 23:31:29 +0100

    feat: case 7 done

--- starter-code/main.js
+++ starter-code/main.js
@@ -86,6 +86,28 @@ mongoClient.connect(url, (error, db) => {
 									}
 									})
 							break;
+							case "6":
+								db.collection('companies').find({"offices.city": "Barcelona"},{name: 1, city:1, _id: 0}).toArray((error, result) => {
+										if (error) {
+										console.log(error);
+										rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+										} else {
+										console.log(result);
+										rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+										}
+										})
+								break;
+								case "7":
+									db.collection('companies').find({},{name: 1, number_of_employees:1, _id: 0}).limit(10).sort({ number_of_employees: -1 }).toArray((error, result) => {
+											if (error) {
+											console.log(error);
+											rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+											} else {
+											console.log(result);
+											rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+											}
+											})
+									break;
           case "0":
             console.log(`👋👋👋👋 😞 \n`);
             db.close((error) => { process.exit(0) });
