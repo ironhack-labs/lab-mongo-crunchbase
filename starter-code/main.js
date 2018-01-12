cfffc958d659fd86a48c55ba3d2a2b63dad65f2d
Cristina Bertrand <cristina@11870.com>
2018-01-12 20:46:47 +0100

    feat: until case 4 done

--- starter-code/main.js
+++ starter-code/main.js
@@ -31,21 +31,61 @@ mongoClient.connect(url, (error, db) => {
       printMenu();
       rl.question('Type an option: ', (option) => {
         switch(option){
-          case "1":
-            db.collection('companies').find({}, {name: 1, _id: 0}).toArray((error, result) => {
-                if (error) {
-                  console.log(error);
-                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
-                } else {
-                  console.log(result);
-                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
-                }
-                })
-            break;
-          case "2":
-            console.log('you typed 2');
-            rl.question(`\nType enter to continue: `, (answer) => {mainMenu()});
-            break;
+					case "1":
+							db.collection('companies').find({}, {name: 1, _id: 0}).toArray((error, result) => {
+								if (error) {
+									console.log(error);
+									rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+								} else {
+									console.log(result);
+									rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+								}
+								})
+						break;
+					case "2":
+							db.collection('companies').count((error, result) => {
+									if (error) {
+									console.log(error);
+									rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+									} else {
+									console.log(result);
+									rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+									}
+									})
+						break;
+					case "3":
+							db.collection('companies').find({"founded_year": {"$eq": 2004}},{name: 1, _id: 0}).count((error, result) => {
+									if (error) {
+									console.log(error);
+									rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+									} else {
+									console.log(result);
+									rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+									}
+									})
+						break;
+					case "4":
+							db.collection('companies').find({"founded_year": 2004, "founded_month": {"$eq": 2}},{name: 1, _id: 0}).toArray((error, result) => {
+									if (error) {
+									console.log(error);
+									rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+									} else {
+									console.log(result);
+									rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+									}
+									})
+						break;
+						case "5":
+							db.collection('companies').find({{"founded_year": 2004}, { $or: [ {"founded_month": 4}, {"founded_month": 5} ] }},{name: 1, _id: 0}).sort().toArray((error, result) => {
+									if (error) {
+									console.log(error);
+									rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+									} else {
+									console.log(result);
+									rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+									}
+									})
+							break;
           case "0":
             console.log(`👋👋👋👋 😞 \n`);
             db.close((error) => { process.exit(0) });
