7843db04f59f7af1e1d64455e74bd8b55f0a4f74
Ana Recuero <anarecsa@gmail.com>
2018-01-31 09:38:04 +0100

    query 14

--- starter-code/main.js
+++ starter-code/main.js
@@ -169,6 +169,17 @@ mongoClient.connect(url, (error, db) => {
               }
             })
           break;
+          case "14":
+            db.collection('companies').find({name: "Facebook"},{"competitions.competitor.name":1,_id: 0}).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              } else {
+                console.log(result[0].competitions);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              }
+            })
+          break;
           case "0":
             console.log(`👋👋👋👋 😞 \n`);
             db.close((error) => { process.exit(0) });
